8bc3ba24f4291d0880d8952fb3c032cfcba9f6f8
DiamondGotCat <diamondgotcat@gmail.com>
2025-06-06 15:46:37 +0900

    Update container.py

--- allowdynamic/container.py
+++ allowdynamic/container.py
@@ -5,12 +5,16 @@
 from typing import Callable, Dict, Any
 
 class DynamicContainer:
-    def __init__(self, action: Callable[[Any], Any]):
+    def __init__(self, action: Callable[[Any], Any], cacheMode: bool = True, sliceLimit: int = 1_000_000):
         self.cache: Dict[Any, Any] = {}
+        self.cacheMode = cacheMode
         self.system = {"action": action}
+        self.sliceLimit = sliceLimit
 
     def __getitem__(self, key: Any):
-        if key in self.cache:
+        if isinstance(key, slice):
+            return [self[i] for i in range(*key.indices(self.sliceLimit))]
+        elif (key in self.cache) and self.cacheMode:
             return self.cache[key]
         else:
             value = self.system["action"](key)
@@ -18,10 +22,29 @@ class DynamicContainer:
             return value
 
     def __contains__(self, key):
+        return (key in self.cache) if self.cacheMode else False
+        
+    def __repr__(self):
+        return f"<DynamicContainer: {len(self.cache)} cached entries (use cache: {'enabled' if self.cacheMode else 'disabled'})>"
+    
+    def __len__(self):
+        return len(self.cache)
+    
+    def setCacheMode(self, enable: bool = True):
+        self.cacheMode = enable
+    
+    def isCached(self, key):
         return key in self.cache
+    
+    def updateCache(self, key, value):
+        self.cache[key] = value
+    
+    def removeFromCache(self, key):
+        self.cache.pop(key, None)
+
+    def clearCache(self):
+        self.cache.clear()
 
     def newAction(self, action: Callable[[Any], Any]):
         self.system["action"] = action
 
-    def clearCache(self):
-        self.cache.clear()
